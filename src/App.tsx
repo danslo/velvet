c77d2e2c9b80101fec1bb8a4f8bafbd31bb65059
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-09 12:43:10 +0100

    add some material design

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {AuthProvider} from "./state/context";
 import {AppRoutes} from "./AppRoutes";
+import {Box, Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 
 const App = () => {
     return (
         <AuthProvider>
-            <BrowserRouter>
-                <AppRoutes />
-            </BrowserRouter>
+            <ThemeProvider theme={createTheme()}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box sx={{marginTop: 8}}>
+                        <BrowserRouter>
+                            <AppRoutes />
+                        </BrowserRouter>
+                    </Box>
+                </Container>
+            </ThemeProvider>
         </AuthProvider>
     )
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,34 +1,29 @@
-import React, {FormEvent, useState} from "react";
+import React, {FormEvent} from "react";
 import {useAuthDispatch} from "../../state/context";
 import {login} from "../../state/actions";
+import {Avatar, Box, Button, TextField, Typography} from "@mui/material";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 const Login = () => {
   const {dispatch} = useAuthDispatch();
 
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
-
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (username && password) {
-        login(dispatch, {username, password});
-    }
+      e.preventDefault();
+      const data = new FormData(e.currentTarget);
+      const username = data.get('username')?.toString();
+      const password = data.get('password')?.toString();
+      if (username && password) {
+          login(dispatch, {username, password});
+      }
   }
 
   return(
-    <form onSubmit={handleSubmit}>
-      <label>
-        <p>Username</p>
-        <input type="text" onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" onChange={e => setPassword(e.target.value)} />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Typography component="h1" variant="h5">Sign in</Typography>
+          <TextField margin="normal" required fullWidth id="username" label="Username" name="username" autoFocus />
+          <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Sign In</Button>
+      </Box>
   )
 }
 
