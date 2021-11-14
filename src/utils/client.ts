343790d7bda9f6d9f510e5f4bad237979d080f88
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-14 11:29:21 +0100

    fix auth link

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -5,16 +5,16 @@ export const client = new ApolloClient({cache: new InMemoryCache({})});
 
 export const setClientLink = (token: string | null) => {
     const httpLink = new HttpLink({uri: process.env.REACT_APP_BACKEND_URL});
-
     if (token) {
-        client.setLink(httpLink.concat(setContext((_, {headers}) => {
+        const authLink = setContext((_, {headers}) => {
             return {
                 headers: {
                     ...headers,
                     authorization: `Bearer ${token}`,
                 }
             }
-        })));
+        });
+        client.setLink(authLink.concat(httpLink));
     } else {
         client.setLink(httpLink);
     }
