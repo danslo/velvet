3cad561adc942a8d251be4bc9ea7b1ce94301125
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-15 15:26:58 +0100

    clean up navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,135 +1,51 @@
 import * as React from 'react';
-import {styled, useTheme} from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import MuiAppBar, {AppBarProps as MuiAppBarProps} from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import LogoutIcon from '@mui/icons-material/Logout';
-import HomeIcon from '@mui/icons-material/Home';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import ConfigIcon from '@mui/icons-material/Build';
 import {Link, useNavigate} from "react-router-dom";
 import {useAuthDispatch} from "../../context/auth.context";
 import {logout} from "../../actions/auth.actions";
-
-const drawerWidth = 240;
-
-interface AppBarProps extends MuiAppBarProps {
-    open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({theme, open}) => ({
-    transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: `${drawerWidth}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
-}));
-
-const DrawerHeader = styled('div')(({theme}) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-}));
-
+import {AppBar} from "@mui/material";
 
 export default function Navbar() {
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
     const {dispatch} = useAuthDispatch();
     const navigate = useNavigate();
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
 
     const handleLogout = () => {
         logout(dispatch).then(() => navigate("/"));
     }
 
     return (
-        <Box sx={{display: 'flex'}}>
-            <AppBar position="fixed" open={open}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        sx={{mr: 2, ...(open && {display: 'none'})}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h5" noWrap component="div">
-                        Velvet
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    '& .MuiDrawer-paper': {
-                        width: drawerWidth,
-                        boxSizing: 'border-box',
-                    },
-                }}
-                variant="persistent"
-                anchor="left"
-                open={open}
-            >
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider/>
-                <List>
+        <AppBar position="sticky">
+            <Toolbar>
+                <Typography variant="h5" noWrap component="div" sx={{mr: 2}}>
+                    Velvet
+                </Typography>
+                <List sx={{display: "flex", flexDirection: "row"}}>
                     <ListItem component={Link} to="/dashboard">
-                        <ListItemIcon>
-                            <HomeIcon/>
+                        <ListItemIcon sx={{minWidth: "20px"}}>
+                            <DashboardIcon/>
                         </ListItemIcon>
-                        <ListItemText primary="Dashboard"/>
                     </ListItem>
                     <ListItem component={Link} to="/configuration">
-                        <ListItemIcon>
+                        <ListItemIcon sx={{minWidth: "20px"}}>
                             <ConfigIcon/>
                         </ListItemIcon>
-                        <ListItemText primary="Configuration"/>
                     </ListItem>
                     <Divider/>
                     <ListItem component={Link} to="#" onClick={handleLogout}>
-                        <ListItemIcon>
+                        <ListItemIcon sx={{minWidth: "20px"}}>
                             <LogoutIcon/>
                         </ListItemIcon>
-                        <ListItemText primary="Logout"/>
                     </ListItem>
                 </List>
-            </Drawer>
-            <DrawerHeader/>
-        </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
