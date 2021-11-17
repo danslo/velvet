6c6d7ec265de605a4db3a567718d997680150c92
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-17 10:28:41 +0100

    handle graphql authorization errors

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,23 +1,34 @@
-import {ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
+import {ApolloClient, from, HttpLink, InMemoryCache} from "@apollo/client";
 import {setContext} from "@apollo/client/link/context";
+import {onError} from "@apollo/client/link/error";
 
 export const client = new ApolloClient({cache: new InMemoryCache({})});
 
-export const setClientLink = (token: string | null) => {
-    const httpLink = new HttpLink({uri: process.env.REACT_APP_BACKEND_URL});
-    if (token) {
-        const authLink = setContext((_, {headers}) => {
-            return {
-                headers: {
-                    ...headers,
-                    authorization: `Bearer ${token}`,
-                }
+const httpLink = new HttpLink({uri: process.env.REACT_APP_BACKEND_URL});
+
+const errorLink = onError(({graphQLErrors, networkError}) => {
+    if (graphQLErrors) {
+        graphQLErrors.forEach(({extensions}) => {
+            if (extensions?.category === 'graphql-authorization') {
+                // todo: somehow handle the redirect without relying on react-router-dom here
+                localStorage.removeItem('token');
             }
         });
-        client.setLink(authLink.concat(httpLink));
-    } else {
-        client.setLink(httpLink);
     }
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const authLink = (token: string | null) => setContext((_, {headers}) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: `Bearer ${token}`,
+        }
+    }
+});
+
+export const setClientLink = (token: string | null) => {
+    client.setLink(from([errorLink, authLink(token), httpLink]));
 }
 
 setClientLink(localStorage.getItem('token'));
