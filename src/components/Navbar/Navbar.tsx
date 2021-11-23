d6c1d8d1e8450567a84a3db4d867918d1cda2fa6
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-23 10:30:26 +0100

    fix logout button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useContext} from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
@@ -11,40 +12,50 @@ import {Link} from "react-router-dom";
 import {AppBar} from "@mui/material";
 import CachedIcon from '@mui/icons-material/Cached';
 import StorageIcon from '@mui/icons-material/Storage';
+import {setClientLink} from "../../utils/client";
+import {AuthContext} from "../../utils/auth";
 
-const Navbar = () => (
-    <AppBar position="sticky">
-        <Toolbar>
-            <Typography variant="h5" noWrap component="div" sx={{mr: 2}}>Velvet</Typography>
-            <List sx={{display: "flex", flexDirection: "row"}}>
-                <ListItem component={Link} to="/dashboard">
-                    <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
-                        <DashboardIcon/>
-                    </ListItemIcon>
-                </ListItem>
-                <ListItem component={Link} to="/configuration">
-                    <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
-                        <ConfigIcon/>
-                    </ListItemIcon>
-                </ListItem>
-                <ListItem component={Link} to="/cache">
-                    <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
-                        <CachedIcon/>
-                    </ListItemIcon>
-                </ListItem>
-                <ListItem component={Link} to="/indexers">
-                    <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
-                        <StorageIcon/>
-                    </ListItemIcon>
-                </ListItem>
-                <ListItem component={Link} to="#" onClick={() => undefined}>
-                    <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
-                        <LogoutIcon/>
-                    </ListItemIcon>
-                </ListItem>
-            </List>
-        </Toolbar>
-    </AppBar>
-);
+const Navbar = () => {
+    const {setToken} = useContext(AuthContext);
+    return (
+        <AppBar position="sticky">
+            <Toolbar>
+                <Typography variant="h5" noWrap component="div" sx={{mr: 2}}>Velvet</Typography>
+                <List sx={{display: "flex", flexDirection: "row"}}>
+                    <ListItem component={Link} to="/dashboard">
+                        <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
+                            <DashboardIcon/>
+                        </ListItemIcon>
+                    </ListItem>
+                    <ListItem component={Link} to="/configuration">
+                        <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
+                            <ConfigIcon/>
+                        </ListItemIcon>
+                    </ListItem>
+                    <ListItem component={Link} to="/cache">
+                        <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
+                            <CachedIcon/>
+                        </ListItemIcon>
+                    </ListItem>
+                    <ListItem component={Link} to="/indexers">
+                        <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
+                            <StorageIcon/>
+                        </ListItemIcon>
+                    </ListItem>
+                    <ListItem component={Link} to="#" onClick={() => {
+                        localStorage.removeItem('token');
+                        setClientLink(null);
+                        setToken(null);
+                        window.location.href = '/';
+                    }}>
+                        <ListItemIcon sx={{minWidth: "20px", color: "white"}}>
+                            <LogoutIcon/>
+                        </ListItemIcon>
+                    </ListItem>
+                </List>
+            </Toolbar>
+        </AppBar>
+    )
+}
 
 export default Navbar;
