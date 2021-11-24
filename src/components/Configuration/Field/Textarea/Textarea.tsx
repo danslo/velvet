95926faf06070dd00eb533b5d3045ca61dcad5a6
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-24 14:57:28 +0100

    populate textarea config fields

--- src/components/Configuration/Field/Textarea/Textarea.tsx
+++ src/components/Configuration/Field/Textarea/Textarea.tsx
@@ -1,11 +1,12 @@
 import TextField from '@mui/material/TextField';
 import {FieldComponentProps} from "../Field";
 
-const Textarea = ({disabled, setValue}: FieldComponentProps) => {
+const Textarea = ({disabled, value, setValue}: FieldComponentProps) => {
     return <TextField
         disabled={disabled}
         multiline
         rows={4}
+        value={value}
         onChange={(e) => setValue(e.target.value)}
         variant="standard"/>
 }
