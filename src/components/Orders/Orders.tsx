e5aeb8c08a72a76bbdcb92135ce6ef9652c2aae9
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-25 15:40:04 +0100

    always show pagination

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -2,12 +2,13 @@ import {withLayout} from "../Layout/Layout";
 import {useGetOrderGridQuery} from "../../types";
 import LoaderHandler from "../LoaderHandler/LoaderHandler";
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow} from "@mui/material";
-import React from "react";
+import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 
 const Orders = () => {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [pageCount, setPageCount] = React.useState(100);
     const {data, loading, error} = useGetOrderGridQuery({
         variables: {
             page_size: rowsPerPage,
@@ -29,55 +30,63 @@ const Orders = () => {
         setPage(0);
     };
 
+    useEffect(() => {
+        if (data) {
+            setPageCount(data.orderGrid.last_page_number);
+        }
+    }, [data]);
+
     return (
-        <LoaderHandler loading={loading} error={error}>
-            {data && (
-                <>
-                    <TablePagination
-                        component="div"
-                        count={data.orderGrid.last_page_number}
-                        page={page}
-                        onPageChange={handleChangePage}
-                        rowsPerPage={rowsPerPage}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
-                    <TableContainer component={Paper} sx={{mb: 2}}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Increment ID</TableCell>
-                                    <TableCell>Store</TableCell>
-                                    <TableCell>Created At</TableCell>
-                                    <TableCell>Billing Name</TableCell>
-                                    <TableCell>Shipping Name</TableCell>
-                                    <TableCell>Grand Total</TableCell>
-                                    <TableCell>Status</TableCell>
-                                    <TableCell>Shipping Address</TableCell>
-                                    <TableCell>Actions</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {data.orderGrid.orders.map(order => (
+        <>
+            <TablePagination
+                component="div"
+                count={pageCount}
+                page={page}
+                onPageChange={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+            <LoaderHandler loading={loading} error={error}>
+                {data && (
+                    <>
+                        <TableContainer component={Paper} sx={{mb: 2}}>
+                            <Table>
+                                <TableHead>
                                     <TableRow>
-                                        <TableCell>{order.increment_id}</TableCell>
-                                        <TableCell>{order.store_name}</TableCell>
-                                        <TableCell>{order.created_at}</TableCell>
-                                        <TableCell>{order.billing_name}</TableCell>
-                                        <TableCell>{order.shipping_name}</TableCell>
-                                        <TableCell>{order.grand_total}</TableCell>
-                                        <TableCell>{order.status}</TableCell>
-                                        <TableCell>{order.shipping_address}</TableCell>
-                                        <TableCell>
-                                            <Link to={"/orders/" + order.increment_id}>View</Link>
-                                        </TableCell>
+                                        <TableCell>Increment ID</TableCell>
+                                        <TableCell>Store</TableCell>
+                                        <TableCell>Created At</TableCell>
+                                        <TableCell>Billing Name</TableCell>
+                                        <TableCell>Shipping Name</TableCell>
+                                        <TableCell>Grand Total</TableCell>
+                                        <TableCell>Status</TableCell>
+                                        <TableCell>Shipping Address</TableCell>
+                                        <TableCell>Actions</TableCell>
                                     </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </>
-            )}
-        </LoaderHandler>
+                                </TableHead>
+                                <TableBody>
+                                    {data.orderGrid.orders.map(order => (
+                                        <TableRow>
+                                            <TableCell>{order.increment_id}</TableCell>
+                                            <TableCell>{order.store_name}</TableCell>
+                                            <TableCell>{order.created_at}</TableCell>
+                                            <TableCell>{order.billing_name}</TableCell>
+                                            <TableCell>{order.shipping_name}</TableCell>
+                                            <TableCell>{order.grand_total}</TableCell>
+                                            <TableCell>{order.status}</TableCell>
+                                            <TableCell>{order.shipping_address}</TableCell>
+                                            <TableCell>
+                                                <Link to={"/orders/" + order.increment_id}>View</Link>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </>
+                )}
+            </LoaderHandler>
+        </>
     );
 }
 
