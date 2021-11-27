7803805c6f1b70cc0429b1bc950710273369f391
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-27 15:04:05 +0100

    load the order

--- src/components/Orders/View/View.tsx
+++ src/components/Orders/View/View.tsx
@@ -1,11 +1,26 @@
 import {useParams} from "react-router-dom";
 import {withLayout} from "../../Layout/Layout";
+import {useGetOrderViewQuery} from "../../../types";
+import LoaderHandler from "../../LoaderHandler/LoaderHandler";
+import {Box, Paper} from "@mui/material";
 
 const View = () => {
     const {orderId} = useParams();
 
+    const {data, loading, error} = useGetOrderViewQuery({
+        variables: {
+            id: parseInt(orderId!)
+        }
+    });
+
     return (
-        <h1>Order {orderId}</h1>
+        <LoaderHandler loading={loading} error={error}>
+            {data && (
+                <Box component={Paper}>
+                    <h1>{data.orderView.number}</h1>
+                </Box>
+            )}
+        </LoaderHandler>
     )
 }
 
