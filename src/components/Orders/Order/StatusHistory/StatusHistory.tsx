fd1b23018d9fbbf3dc3b94a0b93eb273ee765264
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-29 16:07:11 +0100

    adding notes dividers

--- src/components/Orders/Order/StatusHistory/StatusHistory.tsx
+++ src/components/Orders/Order/StatusHistory/StatusHistory.tsx
@@ -1,4 +1,4 @@
-import {Box, List, ListItemText, Paper, Typography} from "@mui/material";
+import {Box, Divider, List, ListItemText, Paper, Typography} from "@mui/material";
 import {OrderStatus} from "../../../../types";
 
 type NotesProps = {
@@ -11,14 +11,15 @@ const StatusHistory = ({caption, status_history}: NotesProps) => (
         <Typography variant="h6">{caption}</Typography>
         <List>
             {status_history.map(status => (
-                <>
+                <Box sx={{mb: 2}}>
                     <ListItemText sx={{textTransform: "capitalize"}} primary={status.status}
                                   secondary={status.created_at}/>
                     <ListItemText primary={"Customer Notified: " + (status.customer_notified ? "Yes" : "No")}/>
                     {status.comment && (
                         <ListItemText primary={status.comment}/>
                     )}
-                </>
+                    <Divider/>
+                </Box>
             ))}
         </List>
     </Box>
