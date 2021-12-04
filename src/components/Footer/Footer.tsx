a14aeffb3193280b6f2a44f3ed1476d424ca08dd
Daniel Sloof <daniel@wearejh.com>
2021-12-04 21:11:15 +0100

    always have footer at the bottom

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import {AppBar, Container, Toolbar, Typography} from "@mui/material";
 
 const Footer = () => (
-    <AppBar position="static">
+    <AppBar position="static" sx={{marginTop: "auto"}}>
         <Container maxWidth={false}>
             <Toolbar>
                 <Typography variant="body1" color="inherit">Copyright &copy; 2021 Velvet Commerce Inc. All rights
