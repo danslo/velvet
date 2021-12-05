70c077395ea02700b32eecb030111e5520b61756
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-05 22:45:07 +0100

    tmp block form

--- src/components/Blocks/Block/Block.tsx
+++ src/components/Blocks/Block/Block.tsx
@@ -1,11 +1,48 @@
 import {withLayout} from "../../../hocs/layout";
 import {useParams} from "react-router-dom";
+import {FormControl, FormControlLabel, FormGroup, Input, InputLabel, Switch} from "@mui/material";
 
 const Block = () => {
     const {blockId} = useParams();
 
+    /**
+     * +---------------+--------------+------+-----+---------------------+-------------------------------+
+     | Field         | Type         | Null | Key | Default             | Extra                         |
+     +---------------+--------------+------+-----+---------------------+-------------------------------+
+     | block_id      | smallint(6)  | NO   | PRI | NULL                | auto_increment                |
+     | title         | varchar(255) | NO   | MUL | NULL                |                               |
+     | identifier    | varchar(255) | NO   |     | NULL                |                               |
+     | content       | mediumtext   | YES  |     | NULL                |                               |
+     | creation_time | timestamp    | NO   |     | current_timestamp() |                               |
+     | update_time   | timestamp    | NO   |     | current_timestamp() | on update current_timestamp() |
+     | is_active     | smallint(6)  | NO   |     | 1                   |                               |
+     +---------------+--------------+------+-----+---------------------+-------------------------------+
+
+     */
+
     return (
-        <>{blockId}</>
+        <div>
+            <FormControl>
+                <InputLabel htmlFor="title">Title</InputLabel>
+                <Input id="title"/>
+            </FormControl>
+            <br/><br/>
+            <FormControl>
+                <InputLabel htmlFor="identifier">Identifier</InputLabel>
+                <Input id="identifier"/>
+            </FormControl>
+            <br/><br/>
+            <FormControl>
+                <InputLabel htmlFor="content">Content</InputLabel>
+                <Input id="content"/>
+            </FormControl>
+            <br/><br/>
+            <FormControl>
+                <FormGroup>
+                    <FormControlLabel control={<Switch defaultChecked/>} label="Active"/>
+                </FormGroup>
+            </FormControl>
+        </div>
     )
 }
 
