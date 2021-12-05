c70cf8d1295d8b3802376a9c60cb916ed040aa7d
Daniel Sloof <daniel@wearejh.com>
2021-12-05 01:11:24 +0100

    cache menu context

--- src/components/Cache/Cache.tsx
+++ src/components/Cache/Cache.tsx
@@ -1,11 +1,32 @@
 import {withLayout} from "../Layout/Layout";
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Menu, MenuItem, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import React from "react";
 import {useGetCacheTypesQuery} from "../../types";
 import LoaderHandler from "../LoaderHandler/LoaderHandler";
 
 const Cache = () => {
     const {data, loading, error} = useGetCacheTypesQuery();
+    const [contextMenu, setContextMenu] = React.useState<{
+        mouseX: number;
+        mouseY: number;
+        type: string;
+    } | null>(null);
+
+    const handleContextMenu = (event: React.MouseEvent, cacheType: string) => {
+        event.preventDefault();
+
+        setContextMenu(
+            contextMenu === null ? {
+                mouseX: event.clientX - 2,
+                mouseY: event.clientY - 4,
+                type: cacheType
+            } : null,
+        );
+    };
+
+    const handleClose = () => {
+        setContextMenu(null);
+    };
 
     return (
         <LoaderHandler loading={loading} error={error}>
@@ -22,13 +43,21 @@ const Cache = () => {
                         </TableHead>
                         <TableBody>
                             {data.cacheTypes.map(cacheType => (
-                                <TableRow hover={true}>
+                                <TableRow hover={true} onClick={(event: React.MouseEvent) => handleContextMenu(event, cacheType.cache_type)} sx={{cursor: "pointer"}}>
                                     <TableCell>{cacheType.cache_type}</TableCell>
                                     <TableCell>{cacheType.description}</TableCell>
                                     <TableCell>{cacheType.tags}</TableCell>
                                     <TableCell>{cacheType.status}</TableCell>
                                 </TableRow>
                             ))}
+                            <Menu
+                                open={contextMenu !== null}
+                                onClose={handleClose}
+                                anchorReference="anchorPosition"
+                                anchorPosition={contextMenu !== null ? { top: contextMenu.mouseY, left: contextMenu.mouseX } : undefined}>
+                                <MenuItem onClick={handleClose}>Enable</MenuItem>
+                                <MenuItem onClick={handleClose}>Disable</MenuItem>
+                            </Menu>
                         </TableBody>
                     </Table>
                 </TableContainer>
