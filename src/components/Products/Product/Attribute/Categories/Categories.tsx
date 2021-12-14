f7c90885c9742dcc67e156ea9ae667785344eb3e
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-14 17:14:19 +0100

    category tree component

--- src/components/Products/Product/Attribute/Categories/Categories.tsx
+++ src/components/Products/Product/Attribute/Categories/Categories.tsx
@@ -1,9 +1,48 @@
 import React, {FunctionComponent} from "react";
 import {AttributeProps} from "../Attribute";
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Category from "./Category/Category";
 
 const Categories: FunctionComponent<AttributeProps> = props => {
+    const [expanded, setExpanded] = React.useState<string[]>(
+        [props.product.categories![0].id!.toString()]
+    );
+    const [selected, setSelected] = React.useState<string[]>(
+        props.product.category_ids!.map(id => id!.toString())
+    );
+
+    const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
+        event.persist()
+        let iconClicked = (event.target as Element).closest(".MuiTreeItem-iconContainer")
+        if (iconClicked) {
+            setExpanded(nodeIds);
+        }
+    };
+
+    const handleSelect = (event: React.SyntheticEvent, nodeIds: string[]) => {
+        event.persist()
+        let iconClicked = (event.target as Element).closest(".MuiTreeItem-iconContainer")
+        if (!iconClicked) {
+            setSelected(nodeIds);
+        }
+    };
+
     return (
-        <>TODO</>
+        <TreeView
+            onNodeToggle={handleToggle}
+            onNodeSelect={handleSelect}
+            expanded={expanded}
+            selected={selected}
+            multiSelect
+            defaultCollapseIcon={<ExpandMoreIcon/>}
+            defaultExpandIcon={<ChevronRightIcon/>}
+            sx={{height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto'}}>
+            {props.product.categories?.map(category => (
+                <Category category={category}/>
+            ))}
+        </TreeView>
     );
 }
 
---
+++ src/components/Products/Product/Attribute/Categories/Category/Category.tsx
@@ -0,0 +1,16 @@
+import {CategoryTree, Maybe} from "../../../../../../types";
+import TreeItem from '@mui/lab/TreeItem';
+
+type CategoryProps = {
+    category: Maybe<CategoryTree>
+}
+
+const Category = (props: CategoryProps) => (
+    <TreeItem nodeId={props.category!.id!.toString()} label={props.category!.name}>
+        {props.category!.children!.map(child => (
+            <Category category={child}/>
+        ))}
+    </TreeItem>
+)
+
+export default Category;
