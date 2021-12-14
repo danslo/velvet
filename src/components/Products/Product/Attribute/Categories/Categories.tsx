e262db71bca9eb5db1b037df5fa8aa7a24c5e282
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-14 17:36:38 +0100

    tmp

--- src/components/Products/Product/Attribute/Categories/Categories.tsx
+++ src/components/Products/Product/Attribute/Categories/Categories.tsx
@@ -4,14 +4,16 @@ import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Category from "./Category/Category";
+import {Controller} from "react-hook-form";
+import {TextField} from "@mui/material";
 
 const Categories: FunctionComponent<AttributeProps> = props => {
+    const defaultCategories = props.product.category_ids!.map(id => id!.toString());
+
     const [expanded, setExpanded] = React.useState<string[]>(
         [props.product.categories![0].id!.toString()]
     );
-    const [selected, setSelected] = React.useState<string[]>(
-        props.product.category_ids!.map(id => id!.toString())
-    );
+    const [selected, setSelected] = React.useState<string[]>(defaultCategories);
 
     const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
         event.persist()
@@ -30,19 +32,27 @@ const Categories: FunctionComponent<AttributeProps> = props => {
     };
 
     return (
-        <TreeView
-            onNodeToggle={handleToggle}
-            onNodeSelect={handleSelect}
-            expanded={expanded}
-            selected={selected}
-            multiSelect
-            defaultCollapseIcon={<ExpandMoreIcon/>}
-            defaultExpandIcon={<ChevronRightIcon/>}
-            sx={{height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto'}}>
-            {props.product.categories?.map(category => (
-                <Category category={category}/>
-            ))}
-        </TreeView>
+        <>
+            <Controller
+                name="category_ids"
+                control={props.control}
+                render={({field: {onChange}}) => (
+                    <TextField value={selected} onChange={e => console.log(e)}/>
+                )}/>
+            <TreeView
+                onNodeToggle={handleToggle}
+                onNodeSelect={handleSelect}
+                expanded={expanded}
+                selected={selected}
+                multiSelect
+                defaultCollapseIcon={<ExpandMoreIcon/>}
+                defaultExpandIcon={<ChevronRightIcon/>}
+                sx={{flexGrow: 1, maxWidth: 400, overflowY: 'auto'}}>
+                {props.product.categories?.map(category => (
+                    <Category category={category}/>
+                ))}
+            </TreeView>
+        </>
     );
 }
 
