a9a9f4fd90074ea08cbfa43c1547941d2ddc785e
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-18 12:22:37 +0100

    restore textarea variant

--- src/components/FieldComponents/Textarea/Textarea.tsx
+++ src/components/FieldComponents/Textarea/Textarea.tsx
@@ -2,7 +2,7 @@ import TextField from '@mui/material/TextField';
 import {FieldComponentProps, textFieldProps} from "../FieldComponents";
 
 const Textarea = (props: FieldComponentProps) => (
-    <TextField {...textFieldProps(props)} multiline rows={4}/>
+    <TextField {...textFieldProps(props)} variant="standard" multiline rows={4}/>
 )
 
 export default Textarea;
