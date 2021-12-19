9ab92b504c05f80785ca02ed8bde543f17b16e0b
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-19 16:03:10 +0100

    blocks header

--- src/components/Blocks/Blocks.tsx
+++ src/components/Blocks/Blocks.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Grid from "./Grid/Grid";
-import {Button} from "@mui/material";
+import {Box, Button, Toolbar, Typography} from "@mui/material";
 import {withLayout} from "../../hocs/layout";
 import {Link} from "react-router-dom";
 
 const Blocks = () => {
     return (
         <>
-            <Button component={Link} to="/blocks/create" variant="contained" color="primary">
-                Add Block
-            </Button>
+            <Toolbar disableGutters variant="dense">
+                <Box display="flex" flexGrow={1}>
+                    <Typography variant="h5">Blocks</Typography>
+                </Box>
+                <Button component={Link} to="/blocks/create" variant="contained" color="primary">Add Block</Button>
+            </Toolbar>
             <Grid/>
         </>
     );
