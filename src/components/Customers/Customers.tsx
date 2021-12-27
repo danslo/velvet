a771bfafb8588f69c832f2805ecb138adf5840f1
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-27 16:36:31 +0100

    cleanup customers component

--- src/components/Customers/Customers.tsx
+++ src/components/Customers/Customers.tsx
@@ -3,13 +3,11 @@ import Header from "../Header/Header";
 import Grid from "./Grid/Grid";
 import {withLayout} from "../../hocs/layout";
 
-const Customers = () => {
-    return (
-        <>
-            <Header text="Customers"/>
-            <Grid/>
-        </>
-    );
-}
+const Customers = () => (
+    <>
+        <Header text="Customers"/>
+        <Grid/>
+    </>
+);
 
 export default withLayout(Customers);
